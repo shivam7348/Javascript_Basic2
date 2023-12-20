c39b18fb3b5f87ce827ba74feca2d99e58cca1ed
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2023-12-20 20:20:08 +0530

    new code

---
+++ 02Basic_javascript/object2.js
@@ -0,0 +1,7 @@
+// const tinderUser = new Object() //singleton objects :
+const tinderUser = {}   //non singleton objects
+ tinderUser.id = "123abc" 
+ tinderUser.name = "Samy"
+ tinderUser.isLoggedIn = False 
+
+console.log(tinderUser);
