7bd79aeb837ff44e6d06dae9d98894699f878d15
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2023-12-22 23:30:13 +0530

    iteration

---
+++ logic.Control/one.js
@@ -0,0 +1,17 @@
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    if (element ==5) {
+  //      console.log("5 is the best number")
+    }
+//    console.log(element);
+    
+}
+
+for (let i = 0; i <=10; i++) {
+    console.log(`Outer Loop Value: ${i}`);
+    for (let j = 0; j <=10; j++) {
+     console.log(`inner Loop value: ${j} and inner loop ${i}`);   
+        
+    }
+    
+}
