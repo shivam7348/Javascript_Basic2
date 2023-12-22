ae5b2b487970524fdc5c56a5e706783da25dd210
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2023-12-22 23:57:06 +0530

    for infinte loop

--- logic.Control/one.js
+++ logic.Control/one.js
@@ -8,10 +8,28 @@ for (let index = 0; index <= 10; index++) {
 }
 
 for (let i = 0; i <=10; i++) {
-    console.log(`Outer Loop Value: ${i}`);
+    //console.log(`Outer Loop Value: ${i}`);
     for (let j = 0; j <=10; j++) {
-     console.log(`inner Loop value: ${j} and inner loop ${i}`);   
+    // console.log(`inner Loop value: ${j} and inner loop ${i}`);   for doin multiplication
+   // console.log(i + '*' + j + '=' + i*j);
         
     }
     
 }
+ let myArrray = ["flash", "batman", "superman"]
+
+ for (let index = 0; index < myArrray.length; index++) {
+    const element = myArrray[index];
+ //   console.log(element)
+    
+ }
+// infinite loop 
+   for (let i = 0; i<10; i++) {
+     console.log( "Infinte Loops")
+   }
+
+
+
+
+
+
