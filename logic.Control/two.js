bfffce8a05bfafdd68b5554ab9afea213998d469
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2023-12-23 22:02:37 +0530

    DO While

---
+++ logic.Control/two.js
@@ -0,0 +1,23 @@
+
+
+// let index = 0
+// while (index <=10) {
+// //console.log(`Value of index is ${index}`);
+//    index = index + 2 
+// }
+
+let myArrray = [ 'flash', 'batman', 'superman']
+let arr = 0
+while ( arr < myArrray.length){
+console.log(`value is ${myArrray[arr]}`);
+arr = arr + 1 
+}
+// // do while loop 
+ let score = 1  
+
+
+
+
+
+
+
