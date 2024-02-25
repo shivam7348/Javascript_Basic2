7deb16dcc15259bf09c6053fcedb19ea91dc7a8e
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-25 21:14:05 +0530

    bollen expression

--- Practice of Javascript/01.js
+++ Practice of Javascript/01.js
@@ -1,6 +1,19 @@
-let score = "23"
-console.log(typeof score);
+let score = null // o  // undefined = Nan
+// console.log(typeof score);
 
 
-let valueNumber = Number(score)
-console.log(typeof valueNumber)
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber)
+
+// "33" => 33 if it's converted then it will bw 33 result
+// "33abc"=> Nan Not a number
+// true => 1 false; 0
+
+
+let isLoggedIn = "shiv"
+let bolleanisLoggedIn = Boolean(isLoggedIn)
+console.log(bolleanisLoggedIn);    // it shows the boolean value is true or not
+
+// 1 => true 0=> false
+// "" => false
+// "shiva" => true
