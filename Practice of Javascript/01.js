b03b1562a396c918fe2f6ec1f7f86a15c7f23134
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-25 21:49:24 +0530

    iNvestigation

--- Practice of Javascript/01.js
+++ Practice of Javascript/01.js
@@ -36,5 +36,22 @@ let str2 = "hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-console.log("1" + 2);
+// console.log("1" + 2);
+// console.log("1" + 2);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+
+// console.log( +true);
+// console.log( +"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
 
