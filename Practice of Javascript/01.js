c25e291481dd9b111c38172efd50fe07cf8d3414
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-25 21:25:01 +0530

    Opertaion handiling in the javascript

--- Practice of Javascript/01.js
+++ Practice of Javascript/01.js
@@ -20,4 +20,21 @@ let bolleanisLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
  let stringNumber = String(someNumber)
- console.log( typeof stringNumber);
+// console.log( typeof stringNumber);
+ // ****************************** Operations************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 +2);
+// console.log(2-2);
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 2);
+
