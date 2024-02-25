78a4cd6309c9aeee03b7cdabdc54b21451a9c045
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-25 22:01:42 +0530

    prefix and postfrix

--- Practice of Javascript/01.js
+++ Practice of Javascript/01.js
@@ -53,5 +53,21 @@ num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
 gameCounter++;
-console.log(gameCounter);
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+
+
+
+
+
 
