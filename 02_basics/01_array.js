dcc870a103f2b9542943a7aadba3ea048a41486c
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-27 02:44:34 +0530

    join

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,11 +1,11 @@
 // array 
-const myArray = [0, 1, 3, 4, 5 ]
+const myArr = [0, 1, 3, 4, 5 ]
 const myHeroes = ["hsktiman"]
 
 const myArr2 = new Array(1,2,3,4)
 
 
-console.log(myArray[1])
+// console.log(myArray[1])
 
 // Array methods
 // myArray.push(6)
@@ -16,11 +16,15 @@ console.log(myArray[1])
 // myArray.unshift(9)
 // myArray.shift()
 
-console.log(myArray.includes(9));
-console.log(myArray);
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(3));
+// console.log(myArray);
 
 
+const newArray = myArr.join()
+console.log(myArr);
 
+console.log(newArray);
 
 
 
