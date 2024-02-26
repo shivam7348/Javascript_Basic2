3868b64a3cc7b9c5f023d14d5316103839ac9be4
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-26 15:50:58 +0530

    Math math.abs

--- Practice of Javascript/06_nums_and_math.js
+++ Practice of Javascript/06_nums_and_math.js
@@ -22,7 +22,9 @@ console.log(Math.abs(-4))
 console.log(Math.round(4.6))
 console.log(Math.ceil(4.5))
 console.log(Math.floor(4.9))
-console.log(Math.min(4.9))
+console.log(Math.min(4, 5, 6, 9))
+console.log(Math.max(4, 3, 6, 8));
+
 
 
 
