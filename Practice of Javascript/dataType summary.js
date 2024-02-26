95f099ceb5d9004e98db800b480adc4fb9e94823
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-26 07:40:08 +0530

    dataTypesSummary

---
+++ Practice of Javascript/dataType summary.js
@@ -0,0 +1,18 @@
+//primitive Data types 
+// 7 types : String, Number, Bollearn, null, undefined, Symbol BigInt
+
+// Refrence (Not primitive)
+// Arrray, objects , functions
+
+const score = 100
+const scoreValue = 10.2
+
+const isLoggedIn = false
+
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
