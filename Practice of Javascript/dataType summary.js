b84d119ecc635d6cac3bc1bdd227ca552ed624c1
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-26 07:45:46 +0530

    non Primitive Dat a Types

--- Practice of Javascript/dataType summary.js
+++ Practice of Javascript/dataType summary.js
@@ -1,9 +1,5 @@
 //primitive Data types 
 // 7 types : String, Number, Bollearn, null, undefined, Symbol BigInt
-
-// Refrence (Not primitive)
-// Arrray, objects , functions
-
 const score = 100
 const scoreValue = 10.2
 
@@ -16,3 +12,16 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 console.log(id === anotherId);
+
+
+ const bigNumber = 356789033676865685n
+
+
+// Refrence type (Not primitive)
+// Arrray, objects , functions
+
+const heros = ["shaktiman", "nagraj", "doga", ]
+let myObj ={
+    name: "shivam",
+    age: 34,
+}
