bf49d795465e9db4056184753333a4ee4de954c2
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-26 15:43:56 +0530

    abs round ceil floor

--- Practice of Javascript/06_nums_and_math.js
+++ Practice of Javascript/06_nums_and_math.js
@@ -18,3 +18,14 @@ console.log(hundreds.toLocaleString('en-IN'))
 //----------------------***********************************
 console.log(Math)
 console.log(Math.abs(-4))
+
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.5))
+console.log(Math.floor(4.9))
+
+
+
+
+
+
+
