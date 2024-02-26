5528dc9ff9dd0473bd68dd31cb34d1df32381f74
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-26 10:25:33 +0530

    slice

--- Practice of Javascript/05 strings.js
+++ Practice of Javascript/05 strings.js
@@ -8,14 +8,23 @@ const repCount = 60
 
 // console.log(`Hello my name is ${name} and my repo account is ${repCount}`);
 
-const gameName = new String('hiteshhc')
-console.log(gameName[0]);
-console.log(gameName.__prot__);
+const gameName = new String('hitesh-hc')
+// console.log(gameName[0]);
+// console.log(gameName.__prot__);
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+//
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(0, 4)
+console.log(anotherString);
+
+
 
 
 
