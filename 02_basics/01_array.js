d86877abfd3e6d9856efa24353a4aeb62fb00f51
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-26 23:58:58 +0530

    push to add the value in the aaray

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -3,7 +3,15 @@ const myArray = [0, 1, 3, 4, 5, 6, 7 ]
 const myHeroes = ["hsktiman"]
 
 const myArr2 = new Array(1,2,3,4)
+
+
+console.log(myArray[1])
+
+// Array methods
+myArr2.push(6)
 console.log(myArr2);
 
-console.log(myArray[0])
+
+
+
 
