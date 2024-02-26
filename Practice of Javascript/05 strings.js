720f50fcde60b6b519272617a3dcbe09bb031fca
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-26 09:04:29 +0530

    .length proto

--- Practice of Javascript/05 strings.js
+++ Practice of Javascript/05 strings.js
@@ -6,4 +6,10 @@ const repCount = 60
 
 // console.log(name + repCount + "value");
 
-console.log(`Hello my name is ${name} and my repo account is ${repCount}`);
+// console.log(`Hello my name is ${name} and my repo account is ${repCount}`);
+
+const gameName = new String('shivv')
+console.log(gameName[0]);
+console.log(gameName.__prot__);
+
+console.log(gameName.length);
