0965c0ded4388fe44a64703808dfeb6bebe1a6f7
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-27 02:55:27 +0530

    slice and spice method

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -21,13 +21,24 @@ const myArr2 = new Array(1,2,3,4)
 // console.log(myArray);
 
 
-const newArray = myArr.join()
-console.log(myArr);
+// const newArray = myArr.join()
+// console.log(myArr);
 
-console.log(newArray);
+// console.log(newArray);
 
+// console.log(typeof newArray);
 
+// slice , splice
 
+console.log("A ", myArr);
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+console.log("B", myArr);
+
+const myN2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myN2);
 
 // console.log();
 
