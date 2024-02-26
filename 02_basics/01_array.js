c7123b070043bf20829d8561442fcdc67818702d
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-27 00:06:33 +0530

    unshift add the value from starting

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -8,9 +8,21 @@ const myArr2 = new Array(1,2,3,4)
 console.log(myArray[1])
 
 // Array methods
-myArray.push(6)
-myArray.push(7)
-myArray.pop()
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+
+// myArray.unshift(9)
+
+
+
+
+
+
+
+
+// console.log();
 
 
 
