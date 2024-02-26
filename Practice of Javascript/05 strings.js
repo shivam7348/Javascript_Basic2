44dc9c6be2375588a330d33c056fd68a75ea12c3
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-26 10:42:12 +0530

    spilit method in strings

--- Practice of Javascript/05 strings.js
+++ Practice of Javascript/05 strings.js
@@ -8,7 +8,7 @@ const repCount = 60
 
 // console.log(`Hello my name is ${name} and my repo account is ${repCount}`);
 
-const gameName = new String('hitesh-hc')
+const gameName = new String('hitesh-hc-sc')
 // console.log(gameName[0]);
 // console.log(gameName.__prot__);
 
@@ -26,9 +26,15 @@ console.log(anotherString);
 // trim remove the spaces
 const newStringOne = "  hitesh   "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());   // take the return vale
 
+// .replace
+ const url = "https://github.com/shivam7348?tab=overview&from=2023-12-01&to=2023-12-31"
+console.log(url.replace('github.com', '@gmail.com'))
 
+console.log(url.includes('hitesh'))
+
+console.log(gameName.split('-'));
 
 
 
