0b0ec9429d3fd190fc06f0b92608df76c4693f3e
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-26 08:50:17 +0530

    heap and stack summary

--- Practice of Javascript/Stack Heap.js
+++ Practice of Javascript/Stack Heap.js
@@ -4,3 +4,22 @@
 
 
 // In Heap Memory, {Non Primitive are being used} used   Call by Refrence
+
+let myYotube = "hiteshchoudhary"
+let anothername = myYotube
+
+anothername = "chai aur code "
+
+// console.log( myYotube);
+// console.log(anothername);
+
+let userOne = {
+    email: "user#@google",
+    upi: "sachinmax7348@gmail.com"
+}
+
+let userTwo = userOne
+
+userTwo.email ="google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
