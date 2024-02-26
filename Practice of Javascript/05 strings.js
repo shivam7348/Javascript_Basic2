12d238355913f76d4fff488f4f57b82464c53eb5
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-26 08:58:28 +0530

    ``String Interpolation ${}

--- Practice of Javascript/05 strings.js
+++ Practice of Javascript/05 strings.js
@@ -1,2 +1,9 @@
-let myVar = "hello" +" world"
-console.log(myVar)
+// let myVar = "hello" +" world"
+// console.log(myVar)
+
+const name = "hitesh"
+const repCount = 60
+
+// console.log(name + repCount + "value");
+
+console.log(`Hello my name is ${name} and my repo account is ${repCount}`);
