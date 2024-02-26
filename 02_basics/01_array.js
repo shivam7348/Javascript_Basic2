e89ddfce2b082f1aad7bfc3e81343271244ffa1f
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-26 23:53:25 +0530

    arrays and its method

---
+++ 02_basics/01_array.js
@@ -0,0 +1,9 @@
+// array 
+const myArray = [0, 1, 3, 4, 5, 6, 7 ]
+const myHeroes = ["hsktiman"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr2);
+
+console.log(myArray[0])
+
--- Practice of Javascript/06_nums_and_math.js
+++ Practice of Javascript/06_nums_and_math.js
@@ -17,13 +17,13 @@ console.log(hundreds.toLocaleString('en-IN'))
 
 //----------------------***********************************
 // console.log(Math)
-console.log(Math.abs(-4))
+// console.log(Math.abs(-4))
 
-console.log(Math.round(4.6))
-console.log(Math.ceil(4.5))
-console.log(Math.floor(4.9))
-console.log(Math.min(4, 5, 6, 9))
-console.log(Math.max(4, 3, 6, 8));
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4, 5, 6, 9))
+// console.log(Math.max(4, 3, 6, 8));
 
 
 
