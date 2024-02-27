40c433f2f31c117decc67611ac16377a6e604de2
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-27 09:53:44 +0530

    Object Literlas

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,4 +2,22 @@
 // 1. literals =>  2. constructors object => singleton
 
 // SingleTon Object from constructors
+// const JsUser = {
+//     name:name: "Abhishek",
+//     age: 21
+// }
 
+// Object.create  // => constructors method 
+
+// Object Literals
+
+const JsUser ={
+    name: "Abhishek",     // name is string predefined
+    age: 21,
+    location: "JAunpur",
+    email: "sachinmax746@gmail.com",
+    isloggedIn: false,
+    lastLoginDays: []
+
+}
+console.log();
