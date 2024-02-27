4963bf8955cd0acc46a8b3a0133c0177d70d0d33
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-27 09:31:13 +0530

    concat to push the value

---
+++ 02_basics/02_array.js
@@ -0,0 +1,12 @@
+const marvel_heroes = ["thoe", "ironman",'spiderman']
+const dc = [ "batman", "superman", "flash"]
+
+marvel_heroes.push(dc)
+
+// console.log(marvel_heroes)
+// console.log(marvel_heroes[3][1])
+ 
+
+
+marvel_heroes.concat(dc)
+console.log(marvel_heroes);
