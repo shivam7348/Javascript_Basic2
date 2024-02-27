b9de8edeb19e1fe340b22e8c6024ba8392ef9029
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-27 10:31:03 +0530

    Singleton Objects const tinder  = new object()

---
+++ 02_basics/04.object.js
@@ -0,0 +1,12 @@
+// // singleton or COnstructors objects
+// 1 method ****************************
+
+
+// const tinderUser = new Object()    // singleton object
+
+// console.log(tinderUser);
+
+// 2 method //  Non singleTon objects
+
+const tinderUser = {}
+console.log(tinderUser);
