786c3ab05fd8cd2b5411c59ed793a8cba53a8da0
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-28 19:50:15 +0530

    assign the value

--- 02_basics/04.object.js
+++ 02_basics/04.object.js
@@ -28,8 +28,10 @@ const regularUser = {
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
+const obj5 = {5: "c", 4: "d"}
 
-const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = Object.assign({}, obj1, obj2, obj5)
 console.log(obj3);
 
 
