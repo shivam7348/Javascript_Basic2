41b4953544f91aea81237ef304da107714d8a43a
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-28 19:52:36 +0530

    spread Operator obj2, ...obj ...

--- 02_basics/04.object.js
+++ 02_basics/04.object.js
@@ -30,8 +30,10 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
 const obj5 = {5: "c", 4: "d"}
 
+// const obj3 = { obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj5)
 
-const obj3 = Object.assign({}, obj1, obj2, obj5)
+const obj3 = { ...obj1, ...obj2}
 console.log(obj3);
 
 
