992bf2ebf9e45b28322392abe27ae7e43de2a3c3
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-28 19:45:38 +0530

    Object.assign({}0bj 1, obj2 )

--- 02_basics/04.object.js
+++ 02_basics/04.object.js
@@ -24,7 +24,20 @@ const regularUser = {
         }
     }
 }
-console.log(regularUser.fullname?.userfullname.firstname);
+// console.log(regularUser.fullname?.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);
+
+
+
+
+
+
+
 
 
 
