09a3652f779a8ea294d8ea1180dad233b2f0e31a
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-29 09:20:41 +0530

    object destructing

--- 02_basics/04.object.js
+++ 02_basics/04.object.js
@@ -34,7 +34,7 @@ const obj5 = {5: "c", 4: "d"}
 // const obj3 = Object.assign({}, obj1, obj2, obj5)
 
 const obj3 = { ...obj1, ...obj2}
-console.log(obj3);
+// console.log(obj3);
 
 // when the data came from the database that will show by the array of objects
  const users = [
@@ -45,11 +45,25 @@ console.log(obj3);
     }
  ]
 
- console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser)); // it will show all the values entries and keys and values both inside the enteries
+//  console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser)); // it will show all the values entries and keys and values both inside the enteries
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+// Object de-structure and Json API
+ const course = {
+ coursename: "js in hindist",
+ price: "999",
+ courseInstructor: "hitesh",
+
+ }
+
+//  console.log(course.price.length)
+  const {courseInstructor: instructor} = course
+console.log(instructor);
+
+
 
 
 
