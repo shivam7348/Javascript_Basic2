e61fe280a397c4843a8cc272950778427c7e07a4
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-29 12:57:38 +0530

    declaration value

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -8,7 +8,7 @@ let a = 300
  if ( true) {
  let a = 10
  const b = 20
-console.log("INNER: " + a);
+// console.log("INNER: " + a);
  }
 
  for (let i = 0; i < array.length; i++) {
@@ -20,4 +20,36 @@ console.log("INNER: " + a);
 
 //  console.log(a)
 //  console.log(b);
- console.log(c);
+//  console.log(c);
+// nested scope
+
+
+// function one() {
+//     const username = "shiv"
+
+//     function two() {
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     console.log(website);
+//     // two()
+// }
+
+// // one()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
