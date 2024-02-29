33e39a6f2977764abf0624a962a1bd1379159770
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-29 10:45:02 +0530

    return value is a variable which stores the value

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,31 +1,40 @@
-console.log("Hello world");
-console.log("Hello world");
-console.log("Hello world");
-console.log("Hello world");
-console.log("Hello world");
-console.log("Hello world");
-console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
 
 
 function sayName() {
-    console.log("Hello world");
-    console.log("Hello world");
-    console.log("Hello world");
-    console.log("Hello world");
-    console.log("Hello world");
-    console.log("Hello world");
+    // console.log("Hello world");
+    // console.log("Hello world");
+    // console.log("Hello world");
+    // console.log("Hello world");
+    // console.log("Hello world");
+    // console.log("Hello world");
 
 }
 // sayName();
 
-function addTwoNumbers(number1, number2) {   // parameters
-   console.log(number1 + number2) 
-}
-addTwoNumbers( 5, 6 )  
+// function addTwoNumbers(number1, number2) {   // parameters
+//    console.log(number1 + number2) 
+// }
+// addTwoNumbers( 5, 6 )  
 
 //arguments.length
 
 
+function addTwoNumbers (number1, number2) { // parameters) {
+// console.log(number1 + number2);
+// let result = number1 + number2
+// return result
+return number1 + number
+}
+
+const result =addTwoNumbers(3, 5)
+console.log(result)
 
 
 
