5f301f28c07d335eea871fc0e2e611346c1f1cc3
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-29 12:30:40 +0530

    Global Scope and Local scope

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -77,26 +77,27 @@ const user = {
     price: 199
 
 }
- function handleObject(anyObject) {
-console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`); 
-}
+//  function handleObject(anyObject) {
+// console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`); 
+// }
 
 
 // handleObject(user)
 handleObject({
     username: "sam",
-    price
+    price: 399
 
-
-
-
-
-
-
-    
 })
 
+const myNewArray = [200, 400, 100, 600]
 
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 400, 100, 600])); 
 
 
 
---
+++ 03_basics/02_scope.js
@@ -0,0 +1,23 @@
+
+// var c = 300
+
+
+
+
+let a = 300
+ if ( true) {
+ let a = 10
+ const b = 20
+console.log("INNER: " + a);
+ }
+
+ for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+ }
+
+
+
+//  console.log(a)
+//  console.log(b);
+ console.log(c);
