0b9b18f30f5fc0d5dfda5ae629edae191d1a1a23
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-29 13:34:51 +0530

    this context

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,8 +1,11 @@
 const user = {
     username: "Shiva",
-    price: 999
+    price: 999,
     
     welcomeMessage: function() {
-        console.log(`${this.username} welcome to wesite`);
+        console.log(`${this.username}, welcome to wesite`);   // this refer to current context in object
     }
 }
+user.welcomeMessage()
+
+
