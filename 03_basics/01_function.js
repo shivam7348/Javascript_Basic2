2eadcb9b0856cf89bac399573ed19dea30e4c79a
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-29 09:56:59 +0530

    normal function in js

---
+++ 03_basics/01_function.js
@@ -0,0 +1,24 @@
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+
+
+function sayName() {
+    console.log("Hello world");
+    console.log("Hello world");
+    console.log("Hello world");
+    console.log("Hello world");
+    console.log("Hello world");
+    console.log("Hello world");
+
+}
+// sayName();
+
+function addTwoNumbers(number1, number2) {
+   console.log(number1 + number2) 
+}
+addTwoNumbers( 5, 6 )
