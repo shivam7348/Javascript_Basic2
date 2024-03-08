1734c249ff19d069f6688bf39ae9a46d8d36f80a
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-03-08 07:45:49 +0530

    Not Working Properly

---
+++ 04/P.UnlimitedColors/background.js
@@ -0,0 +1,26 @@
+// genrate Colors
+
+const randomColors = function() {
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+
+    for(let i = 0; i < 6; i++) {
+    color =+ hex[Math.floor(Math.random() *16)]  ; 
+   
+}
+
+return color;
+}
+
+const startChangingColor = function() {
+    document.body.style.backgroundColor = randomColors();
+}
+
+const stopChangingColor = function() {
+    
+}
+
+document.querySelector("#start").addEventListener("click", startChangingColor)
+
+
+document.querySelector("#stop").addEventListener("click", stopChangingColor)
