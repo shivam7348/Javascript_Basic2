2e1893c02f76e356edba9cad6735198d8970bab0
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-03-09 17:11:07 +0530

    this

--- 01basics/01_function.js
+++ 01basics/01_function.js
@@ -58,8 +58,8 @@ let val1 = 10
 let val2 = 5
 
 function addNum(num1, num2) {
-let total = num1 + num2
-return total
+  let total = num1 + num2
+   return total
 }
 
 let result1 =  addNum(val1, val2)
