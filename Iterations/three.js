b0efd8cb5590b93378824ea1a99e6fe83e3a71bb
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-03-11 13:05:30 +0530

    for of loop syntx

---
+++ Iterations/three.js
@@ -0,0 +1,54 @@
+// for of 
+
+// array specific loop
+
+["",'']
+[{}, {}, {}]
+
+const arr = [1,2,3,4,5]
+ for (const num  of arr) {
+    // console.log(num);
+
+    
+ }
+
+const greetings = 'Hello World!'
+
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+    
+}
+
+
+// Maps
+// maps is like a array itertaor
+ const map = new Map()
+ map.set('In', "India")
+ 
+ map.set('USA', "United states of america" )
+ map.set('FR', "FRance" )
+  map.set('In', "India")
+
+
+ console.log(map);
+ 
+
+
+ 
+for (const [key, value ]of map) {
+    console.log(key, ':-', value);
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
--- Iterations/two.js
+++ Iterations/two.js
@@ -3,7 +3,19 @@
 // } 
 
 let index = 0
-while (index<= 10){
-    console.log(`value of index${index} `);
+// while (index<= 10){
+//     console.log(`value of index${index} `);
+// index = index + 3
 
+// }
+
+
+
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while(arr< myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr = aar + 1
 }
