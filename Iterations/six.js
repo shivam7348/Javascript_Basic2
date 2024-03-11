5287085b0466d4d888a646eb8e586b6fdbadfd77
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-03-11 19:29:53 +0530

    for each does not take the return value

--- Iterations/six.js
+++ Iterations/six.js
@@ -32,11 +32,47 @@ const myNums = [1,2,3,4,5,6,7,8,9,10 ]
 const coding = ['js', 'css','html', 'javascript']
 
  const values = coding.filter( (item) => {
-console.log(item)
+// console.log(item)
 return item
 })
 
- console.log(values);
+//  console.log(values);
+
+
+
+// const numbers  = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+//  const evenNumbers = numbers.filter(( function(number){
+//     return number % 2 === 0;
+// }));
+
+
+// console.log(evenNumbers);
+
+
+//const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+//numbers.forEach( (number) => {
+    // console.log(number * 2 );
+// })
+//
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+  const  evenNumbers = numbers.filter( (number) => {
+    return number % 2 ===0
+
+ 
+ })
+
+ console.log(evenNumbers);
+
+
+
+
+
+
+
 
 
 
