1eb46993f2e2418b86291549c41b5561dc99c1bb
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-03-11 19:13:31 +0530

    filter and for each differnce filter does return the value but for each does not return the value

--- Iterations/six.js
+++ Iterations/six.js
@@ -1,11 +1,11 @@
 // const coding = ['js', 'css','html', 'javascript']
 
 //  const values = coding.forEach( (item) => {
-// // console.log(item)
+// console.log(item)
 // return item
 // })
 
-// console.log(values);
+//  console.log(values);
 
 
 // for each does not return the value
@@ -17,4 +17,40 @@ const myNums = [1,2,3,4,5,6,7,8,9,10 ]
 
  const newNums = myNums.filter( (num) => num > 4 )
 
- console.log(newNums);
+//  console.log(newNums);
+
+
+// const newCoding =  ["js", "css", "ruby"]
+
+//  const myCoding = newCoding.forEach( (item) => {
+//     console.log(item)
+//     return item
+// })
+ 
+
+
+const coding = ['js', 'css','html', 'javascript']
+
+ const values = coding.filter( (item) => {
+console.log(item)
+return item
+})
+
+ console.log(values);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
