ab1bf8325e7795d1495019f9133ea83a1103f386
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-03-11 13:21:11 +0530

    for in loop

---
+++ Iterations/four.js
@@ -0,0 +1,17 @@
+const myObj = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swift by the apple'
+
+
+}
+
+
+
+for (const key in myObj) {
+console.log(`${key} shortcut id for ${myObj[key]}`);
+}
+// for in loop is working in obj
+
+
