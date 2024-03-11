a3a0c1b161080ded2106901272663d54f5f77ffc
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-03-11 14:14:08 +0530

    for each call back function

---
+++ Iterations/five.js
@@ -0,0 +1,18 @@
+const coding = ['js', 'css','html', 'javascript']
+//  coding.forEach( function (val){
+//     console.log(val);
+//  })
+
+ function printMe(item) {
+    console.log(item);
+
+ }
+
+ coding.forEach(printMe)
+
+
+
+
+// high order function 
+
+
