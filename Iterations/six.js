17962731d253536f8f20951df8e8f24490054ead
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-03-12 11:23:14 +0530

    for each does not  retur the value

--- Iterations/six.js
+++ Iterations/six.js
@@ -1,75 +1,83 @@
+// // const coding = ['js', 'css','html', 'javascript']
+
+// //  const values = coding.forEach( (item) => {
+// // console.log(item)
+// // return item
+// // })
+
+// //  console.log(values);
+
+
+// // for each does not return the value
+
+
+// // filter is the call back function
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10 ]
+
+//  const newNums = myNums.filter( (num) => num > 4 )
+
+// //  console.log(newNums);
+
+
+// // const newCoding =  ["js", "css", "ruby"]
+
+// //  const myCoding = newCoding.forEach( (item) => {
+// //     console.log(item)
+// //     return item
+// // })
+ 
+
+
 // const coding = ['js', 'css','html', 'javascript']
 
-//  const values = coding.forEach( (item) => {
-// console.log(item)
+//  const values = coding.filter( (item) => {
+// // console.log(item)
 // return item
 // })
 
-//  console.log(values);
+// //  console.log(values);
 
 
-// for each does not return the value
+
+// // const numbers  = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+// //  const evenNumbers = numbers.filter(( function(number){
+// //     return number % 2 === 0;
+// // }));
 
 
-// filter is the call back function
-
-const myNums = [1,2,3,4,5,6,7,8,9,10 ]
-
- const newNums = myNums.filter( (num) => num > 4 )
-
-//  console.log(newNums);
+// // console.log(evenNumbers);
 
 
-// const newCoding =  ["js", "css", "ruby"]
+// //const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
-//  const myCoding = newCoding.forEach( (item) => {
-//     console.log(item)
-//     return item
-// })
+// //numbers.forEach( (number) => {
+//     // console.log(number * 2 );
+// // })
+// //
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+//   const  evenNumbers = numbers.filter( (number) => {
+//     return number % 2 ===0
+
  
+//  })
+
+//  console.log(evenNumbers);
 
 
-const coding = ['js', 'css','html', 'javascript']
+// for each does not return the value 
 
- const values = coding.filter( (item) => {
-// console.log(item)
-return item
+
+const coding = [ "js", "css", "cpp","java", "xml"]
+ 
+ const values = coding.forEach((item) => {
+  // console.log(item);
+  return item
 })
-
-//  console.log(values);
-
-
-
-// const numbers  = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-
-//  const evenNumbers = numbers.filter(( function(number){
-//     return number % 2 === 0;
-// }));
-
-
-// console.log(evenNumbers);
-
-
-//const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-
-//numbers.forEach( (number) => {
-    // console.log(number * 2 );
-// })
-//
-
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-  const  evenNumbers = numbers.filter( (number) => {
-    return number % 2 ===0
-
- 
- })
-
- console.log(evenNumbers);
-
-
-
-
+console.log(values);
 
 
 
