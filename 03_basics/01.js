0f85e35dcac112a81ea360087a581e2e38b7612b
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-04-12 22:24:00 +0530

    function rest operations

---
+++ 03_basics/01.js
@@ -0,0 +1,36 @@
+// function addTwo( num1, num2) {
+// console.log(num1 + num2)
+// }
+
+// function addTwoNumbers(num1, num2){
+//     // let result = num1 + num2;
+//     // return result
+//     return num1 + num2;
+
+// }
+
+
+
+//  const result = addTwoNumbers(5, 6)
+//   console.log(result);
+
+// function loginUserMEssage(username) {
+//     return `${username} just logged in`
+// }
+
+// console.log(loginUserMEssage("hitesh"))
+
+function calculatePrice(...num1) {
+    return num1
+}
+
+console.log(calculatePrice(200, 400, 500))
+
+// rest operatior ... it will show al the value in whatever we pass in the function
+
+
+
+
+
+
+
