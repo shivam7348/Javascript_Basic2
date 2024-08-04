78d6de14b7b1591cb7d650d9af8e5e79e9ced6ba
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-08-04 09:38:48 +0530

    starting from thr initial devlopment

---
+++ 01basics/01_test.js
@@ -0,0 +1,18 @@
+const accountId = 14445
+let accountEmail = "hitesh@google"
+var accountPassword = "123456"
+
+   // not allowed accountId = 2
+   accountEmail = "jc@gmail.vcom"
+   accountPassword = "123456787"
+
+
+console.log(accountEmail)
+console.table(accountEmail, accountId) 
+/*
+Prefer not use var
+beacuse of issue in block scope and functional scope
+
+*/
+
+let accontState: 
